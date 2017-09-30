2be892f47632abf260125ac6c7f6a0226bd6e081
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-09-30 22:42:50 +0300

    ChatService - реализация интерфейса службы

--- WcfServiceConsole/ChatService.cs
+++ WcfServiceConsole/ChatService.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace WcfServiceConsole
 {
-	class ChatService: IChatService
+	[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+	class ChatService : IChatService
 	{
+		private List<ChatUser> conectedUsers = new List<ChatUser>();
+		public ChatUser Login(string name)
+		{
+			throw new NotImplementedException();
+		}
 
+		public void Logout(ChatUser name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SendMessage(ChatMessage msg)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
