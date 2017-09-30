29154604828cbbe6d8380eb2df845ee8574c6086
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-09-30 18:33:04 +0300

    Создан сериализуемый класс для сообщения чата - ChatMessage

---
+++ WcfServiceConsole/DataContracts/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace WcfServiceConsole
+{
+	[DataContract]
+	class ChatMessage
+	{
+		[DataMember]
+		public ChatUser User { get; set; }
+		[DataMember]
+		public string Message { get; set; }
+		[DataMember]
+		public DateTime Date { get; set; }
+	}
+}
