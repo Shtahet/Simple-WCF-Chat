b69f38c3572556c5249e7477e7da83512865bb18
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-09-30 16:31:19 +0300

    Создан сериалезируемый класс пользователя чата - ChatUser

---
+++ WcfServiceConsole/DataContracts/ChatUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Net;
+
+namespace WcfServiceConsole.DataContracts
+{
+	[DataContract]
+	class ChatUser
+	{
+		[DataMember]
+		public string NickName { get; set; }
+		public IPAddress NetworkAddress { get; set; }
+		public override string ToString()
+		{
+			return NickName;
+		}
+	}
+}
