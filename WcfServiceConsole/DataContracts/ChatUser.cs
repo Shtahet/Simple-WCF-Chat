7b938ce065a3d78ee15e739a322b17a4cbc17374
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-09-30 16:38:19 +0300

    ChatUser - добавлены атрыбуты для полей

--- WcfServiceConsole/DataContracts/ChatUser.cs
+++ WcfServiceConsole/DataContracts/ChatUser.cs
@@ -13,6 +13,7 @@ namespace WcfServiceConsole.DataContracts
 	{
 		[DataMember]
 		public string NickName { get; set; }
+		[DataMember]
 		public IPAddress NetworkAddress { get; set; }
 		public override string ToString()
 		{
