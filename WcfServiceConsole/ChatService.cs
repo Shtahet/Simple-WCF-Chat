773f24fc680dc7b9d244960331dc7c0bae807463
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-10-02 21:00:17 +0300

    ChatService - написана функция входа в чат

--- WcfServiceConsole/ChatService.cs
+++ WcfServiceConsole/ChatService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Net;
 
 namespace WcfServiceConsole
 {
@@ -13,7 +15,45 @@ namespace WcfServiceConsole
 		private List<ChatUser> conectedUsers = new List<ChatUser>();
 		public ChatUser Login(string name)
 		{
-			throw new NotImplementedException();
+			//Получение IP-адреса клиента
+			OperationContext currContext = OperationContext.Current;
+			MessageProperties msgProp = currContext.IncomingMessageProperties;
+			RemoteEndpointMessageProperty endpoint = msgProp[RemoteEndpointMessageProperty.Name] as RemoteEndpointMessageProperty;
+			IPAddress clientAddress = IPAddress.Parse(endpoint.Address);
+
+			//Проверка наличия адреса в списке подключенных
+			var exists = from e in conectedUsers
+						 where e.NetworkAddress == clientAddress
+						 select e;
+
+			//Добавление нового пользователя
+			if (exists.Count() != 0)
+			{
+				ChatUser newUser = new ChatUser()
+				{
+					NickName = name,
+					NetworkAddress = clientAddress
+				};
+				conectedUsers.Add(newUser);
+
+				//Логирование подключения
+				Console.WriteLine($"Пользователь {newUser.NickName} (IP:{newUser.NickName}) присоединился к чату");
+
+				//Отправка уведомления другим пользователям
+				ChatMessage joinMsg = new ChatMessage()
+				{
+					User = newUser,
+					Message = $"{newUser.NickName} присоединился к чату",
+					Date = DateTime.Now
+				};
+				SendMessage(joinMsg);
+
+				return newUser;
+			}
+			else
+			{
+				return null;
+			}
 		}
 
 		public void Logout(ChatUser name)
