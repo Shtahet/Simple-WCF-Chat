3e0fca7f8da48fd678c88caab379305c0acafddc
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-10-13 23:04:51 +0300

    Добавлено перечесление с действиями пользователя - EnumAction.UserActions

---
+++ WcfServiceConsole/DataContracts/EnumAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceConsole
+{
+	[DataContract]
+	enum UserActions
+	{
+		[EnumMember]
+		Join,
+		[EnumMember]
+		Left
+	}
+}
