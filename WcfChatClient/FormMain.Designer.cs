cb3b402cabfbd3acb462ba5d3410d5ec893303e8
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-10-15 01:45:26 +0300

    Написан обработчик события на кнопку входа/выхода

--- WcfChatClient/FormMain.Designer.cs
+++ WcfChatClient/FormMain.Designer.cs
@@ -108,6 +108,7 @@
 			this.btnConnect.TabIndex = 4;
 			this.btnConnect.Text = "Connect";
 			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
 			// 
 			// txtChat
 			// 
--- WcfChatClient/FormMain.cs
+++ WcfChatClient/FormMain.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WcfChatClient.ChatService;
 
 namespace WcfChatClient
 {
-	public partial class FormMain : Form
+	public partial class FormMain : Form, IChatServiceCallback
 	{
+		private ChatUser Iam;
+		ChatServiceClient proxy;
+
 		public FormMain()
 		{
 			InitializeComponent();
+
 		}
 
 		private void FormMain_Load(object sender, EventArgs e)
@@ -22,5 +27,33 @@ namespace WcfChatClient
 
 		}
 
+		private void btnConnect_Click(object sender, EventArgs e)
+		{
+			var btn = sender as Button;
+			if (btn.Text == "Connect")
+			{
+				btn.Text = "Disconnect";
+				ConnectTo();
+			}
+			else
+			{
+				btn.Text = "Connect";
+				DisconnectFrom();
+			}
+
+		}
+		private void ConnectTo()
+		{
+			if (String.IsNullOrEmpty(txtUserName.Text))
+			{
+				MessageBox.Show("Укажите свой никнейм", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				return;
+			}
+		}
+
+		private void DisconnectFrom()
+		{
+
+		}
 	}
 }
