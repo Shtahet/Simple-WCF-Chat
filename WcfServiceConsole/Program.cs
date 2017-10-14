195eb6c487ab84975d062ce32b1a8081b1c040fd
Alexandr Gergalo (aka Shtahet) <derien001@gmail.com>
2017-10-14 12:35:52 +0300

    Program - создание хостинга для службы

--- WcfServiceConsole/Program.cs
+++ WcfServiceConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace WcfServiceConsole
 {
@@ -10,6 +11,13 @@ namespace WcfServiceConsole
 	{
 		static void Main(string[] args)
 		{
+			ServiceHost host = new ServiceHost(typeof(ChatService));
+			host.Open();
+
+			Console.WriteLine("Сервер чата запущен...");
+			Console.WriteLine("Для завершения нажмите Enter");
+
+			host.Close();
 		}
 	}
 }
